49210c02075b769e683bdc12e15b2747c31cf857
Kawarai-S <kayawakakururu@icloud.com>
2023-05-16 17:55:02 +0900

    joinできた

--- log_app/log_view.php
+++ log_app/log_view.php
@@ -4,14 +4,23 @@ $item_id = $_GET["id"];
 
 //1.DBに接続する（エラー処理追加）*DB接続時はこれをまるっとセットで書けばOK!必要なとこだけ変更してね。
 try{
-    $pdo=new PDO('mysql:dbname=cat_db;charaset=utf8;host=localhost','root','');//host名,ID,パスワード
+    $pdo=new PDO('mysql:dbname=cat_db;charset=utf8;host=localhost','root','');//host名,ID,パスワード
 }catch(PDOException $e){
     exit('データベースに接続出来ませんでした。'.$e->getMessage());
 }
 
 //2.データ取得SQL
-$sql = "SELECT id, item_id, DATE_FORMAT(date, '%Y-%m-%d %H:%i') as f_date, value, checkbox 
-        FROM log_table WHERE item_id=:item_id ORDER BY date DESC";
+// $sql = "SELECT id, item_id, DATE_FORMAT(date, '%Y-%m-%d %H:%i') as f_date, value, checkbox 
+//         FROM log_table WHERE item_id=:item_id ORDER BY date DESC";
+// $stmt = $pdo->prepare($sql);
+// $stmt->bindValue(':item_id',$item_id,PDO::PARAM_INT);
+// $status = $stmt->execute();
+
+$sql = "SELECT * FROM log_table
+        LEFT JOIN item_table
+        ON log_table.item_id = item_table.id
+        WHERE item_table.id = :item_id
+        ORDER BY log_table.date DESC";
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':item_id',$item_id,PDO::PARAM_INT);
 $status = $stmt->execute();
@@ -20,18 +29,18 @@ $status = $stmt->execute();
 $view="";
 if($status==false){
     //SQL実行時にエラーがある場合
-    $erro = $stmt->errorINfo();
+    $error = $stmt->errorINfo();
     exit("QueryError:".$error[2]);
 }else{
     while($result=$stmt->fetch(PDO::FETCH_ASSOC)){
         $view .= '<div class="box">';
         $view .= '<div class="log">';
         $view .= '<a href="log_update.php?id='.$result["id"].'">';
-        $view .= '<div>'.$result["f_date"].'</div>';
+        $view .= '<div>'.$result["date"].'</div>';
         if(!is_null($result["checkbox"])){
             $view .= '<div>'.$result["checkbox"].'</div>';
         }elseif(!is_null($result["value"])){
-            $view .= '<div>'.$result["value"].'</div>';
+            $view .= '<div>'.$result["value"].$result["unit"].'</div>';
         }
         $view .= '</a>';
         $view .= '</div>';
